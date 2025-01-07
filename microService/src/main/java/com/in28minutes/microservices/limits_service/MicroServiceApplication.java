dde7239b9ad875f765c74eab8ad530a86bc3e64d
indhumathi1531 <139542746+indhumathi1531@users.noreply.github.com>
2025-01-07 08:13:28 +0530

    Add files via upload

---
+++ microService/src/main/java/com/in28minutes/microservices/limits_service/MicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.limits_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceApplication.class, args);
+	}
+
+}
